d6bdd654550a40aa4193024db30fa07f02116907
Park Junwoo <ays99111@naver.com>
2024-07-28 18:42:31 +0900

    [Feat] 첫 시작화면 로그인화면으로 변경

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:daliy_routine/screens/loginView.dart';
 import 'package:flutter/material.dart';
+import 'package:kakao_flutter_sdk_user/kakao_flutter_sdk_user.dart';
 import 'screens/calendar_screen.dart';
 import 'screens/routine_screen.dart';
 import 'screens/myinfo_screen.dart';
@@ -6,6 +8,11 @@ import 'shared/navigation.dart';
 import 'splash_screen.dart';
 
 void main() {
+  const kakaoNativeAppKey = "0dae63a7c9c38fec8b7025076280aabe";
+
+  // runApp() 호출 전 Flutter SDK 초기화
+  KakaoSdk.init(nativeAppKey: kakaoNativeAppKey);
+
   runApp(MyApp());
 }
 
@@ -20,7 +27,7 @@ class MyApp extends StatelessWidget {
       ),
       routes: {
         '/splash': (context) => SplashScreen(),
-        '/home': (context) => MainHome(),
+        '/home': (context) => LoginView(),
       },
     );
   }
